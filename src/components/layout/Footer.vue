5ce369f83c87b46e616d6eabb8d5ee8dd6df5771
Mariano Tellaeche <mtellaeche@inversorglobal.com.ar>
2018-03-14 17:12:35 -0300

    First Commit

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+  footer.footer
+    .container
+      .content.has-text-center
+        p Code with love
+</template>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  section.hero.is-success
+    .hero-head
+      header.nav
+        .container
+          .nav-left
+            .nav-item
+              strong Platzi Music
+          .nav-rigth.nav-menu
+
+    .hero-body
+      .container.has-text-centered
+        h1.title Platzi Music
+        h2.subtitle Canciones que estan buenisimas
+</template>

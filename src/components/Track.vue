0c0a30146a7ac61a23fe3dc1f9de6f72d67eb3d3
Mariano Tellaeche <mtellaeche@inversorglobal.com.ar>
2018-03-16 17:55:55 -0300

    Se agregaron las rutas

--- src/components/Track.vue
+++ src/components/Track.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  .card
+  .card(v-if="track && track.album")
     .card-image
       figure.image.is-1by1
         img(:src="track.album.images[0].url")
@@ -20,6 +20,8 @@
           .level-left
             a.level-item
               span.icon.is-small(@click="selectTrack") ▶️
+            a.level-item
+              span.icon.is-small(@click="goToTrack(track.id)") 🔴
 </template>
 
 <script>
@@ -31,6 +33,9 @@ export default {
     selectTrack () {
       this.$emit('select', this.track.id)
       this.$bus.$emit('set-track', this.track)
+    },
+    goToTrack (id) {
+      this.$router.push({ name: 'track', params: { id } })
     }
   }
 }
